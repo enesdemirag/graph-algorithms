2e303ddfdb0e2a2ab893d420045bc6349825574f
Enes Demirağ <ensdmrg@gmail.com>
2021-04-21 12:32:22 +0000

    cleanup

--- q2.cpp
+++ q2.cpp
@@ -21,6 +21,9 @@ public:
     int** adj_matrix;
     void addEdge(Edge& edge);
     void createAdjacencyMatrix();
+    int minDistance(int distances[], bool visited[]);
+    void printPath(int parent[], int node_index);
+    void dijkstra();
 };
 
 void Graph::addEdge(Edge& edge)
@@ -45,13 +48,14 @@ void Graph::createAdjacencyMatrix()
 {
     // Initialize the matrix, allocate memory and set initial values.
     *adj_matrix = new int[nodes.size()];
-    for (int x = 0; x < nodes.size(); x++)
+    for (uint x = 0; x < nodes.size(); x++)
     {
-        adj_matrix[x] = new int[nodes.size()] {0};
+        adj_matrix[x] = new int[nodes.size()];
+        std::fill_n(adj_matrix[x], nodes.size(), 0);
     }
 
     // Put cost values
-    for (int i = 0; i < nodes.size(); i++)
+    for (uint i = 0; i < nodes.size(); i++)
     {
         for (Edge& e : this->edges)
         {
@@ -75,110 +79,69 @@ void Graph::createAdjacencyMatrix()
     }
 }
 
-using namespace std;
-int V = 12;
-
-int minDistance(int distances[], bool visited[])
+int Graph::minDistance(int distances[], bool visited[])
 {
     // Initialize min value
     int min = INT_MAX;
     int min_index = 0;
   
-    for (int v = 0; v < V; v++)
+    for (uint v = 0; v < this->nodes.size(); v++)
     {
         if (visited[v] == false && distances[v] <= min)
         {
-             min = distances[v], min_index = v;
+             min = distances[v];
+             min_index = v;
         }
     }
     return min_index;
 }
 
-std::vector<Node> global_nodes;
-
-void printPath(int parent[], int node_index)
+void Graph::printPath(int parent[], int node_index)
 {
     // If start node reached.
     if (parent[node_index] == - 1)
     {
-        cout << global_nodes[node_index] << " ";
+        std::cout << this->nodes[node_index] << " ";
         return;
     }
-  
+
+    // Recursive call to the parent
     printPath(parent, parent[node_index]);
-    cout << global_nodes[node_index] << " ";
+    std::cout << this->nodes[node_index] << " ";
 }
   
-void dijkstra(int** graph, int src)
+void Graph::dijkstra()
 {
-      
-    // The output array. dist[i]
-    // will hold the shortest
-    // distance from src to i
-    int dist[V]; 
+    int dist[this->nodes.size()]; 
+    bool sptSet[this->nodes.size()];
+    int parent[this->nodes.size()];
   
-    // sptSet[i] will true if vertex
-    // i is included / in shortest
-    // path tree or shortest distance 
-    // from src to i is finalized
-    bool sptSet[V];
-  
-    // Parent array to store
-    // shortest path tree
-    int parent[V];
-  
-    // Initialize all distances as 
-    // INFINITE and stpSet[] as false
-    for (int i = 0; i < V; i++)
+    for (uint i = 0; i < this->nodes.size(); i++)
     {
         parent[0] = -1;
         dist[i] = INT_MAX;
         sptSet[i] = false;
     }
   
-    // Distance of source vertex 
-    // from itself is always 0
-    dist[src] = 0;
+    dist[0] = 0;
   
-    // Find shortest path
-    // for all vertices
-    for (int count = 0; count < V - 1; count++)
+    for (uint count = 0; count < this->nodes.size() - 1; count++)
     {
-        // Pick the minimum distance
-        // vertex from the set of
-        // vertices not yet processed. 
-        // u is always equal to src
-        // in first iteration.
         int u = minDistance(dist, sptSet);
   
-        // Mark the picked vertex 
-        // as processed
         sptSet[u] = true;
   
-        // Update dist value of the 
-        // adjacent vertices of the
-        // picked vertex.
-        for (int v = 0; v < V; v++)
+        for (uint v = 0; v < this->nodes.size(); v++)
   
-            // Update dist[v] only if is
-            // not in sptSet, there is
-            // an edge from u to v, and 
-            // total weight of path from
-            // src to v through u is smaller
-            // than current value of
-            // dist[v]
-            if (!sptSet[v] && graph[u][v] &&
-                dist[u] + graph[u][v] < dist[v])
+            if (!sptSet[v] && this->adj_matrix[u][v] && dist[u] + this->adj_matrix[u][v] < dist[v])
             {
                 parent[v] = u;
-                dist[v] = dist[u] + graph[u][v];
+                dist[v] = dist[u] + this->adj_matrix[u][v];
             } 
     }
   
-    // print the constructed
-    // distance array
-    printPath(parent, V - 1);
-    cout << dist[V - 1];
+    printPath(parent, this->nodes.size() - 1);
+    std::cout << dist[this->nodes.size() - 1];
 }
 
 int main()
@@ -217,8 +180,7 @@ int main()
 
     g.createAdjacencyMatrix();
 
-    global_nodes = g.nodes;
-    dijkstra(g.adj_matrix, 0);
+    g.dijkstra();
 
     return 0;
 }
