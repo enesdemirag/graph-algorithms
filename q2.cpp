3de74bd77545c1b6358dd9adaeb6893b46158b67
Enes Demirağ <ensdmrg@gmail.com>
2021-04-21 09:04:53 +0000

    add dijksra algorithm

--- q2.cpp
+++ q2.cpp
@@ -1,4 +1,3 @@
-#include <climits>
 #include <cstdio>
 #include <vector>
 #include <string>
@@ -45,15 +44,13 @@ void Graph::createAdjacencyMatrix()
 {
     // Initialize the matrix, allocate memory and set initial values.
     *adj_matrix = new int[nodes.size()];
-    for (uint x = 0; x < nodes.size(); x++)
+    for (int x = 0; x < nodes.size(); x++)
     {
-        adj_matrix[x] = new int[nodes.size()];
-        std::fill_n(adj_matrix[x], nodes.size(), 9999);
+        adj_matrix[x] = new int[nodes.size()] {0};
     }
 
-
     // Put cost values
-    for (uint i = 0; i < nodes.size(); i++)
+    for (int i = 0; i < nodes.size(); i++)
     {
         for (Edge& e : this->edges)
         {
@@ -69,12 +66,130 @@ void Graph::createAdjacencyMatrix()
             else if (begin_node == nodes[i])
             {
                 // Find the end node's index.
-                uint j = std::distance(this->nodes.begin(), std::find(this->nodes.begin(), this->nodes.end(), end_node));
+                int j = std::distance(this->nodes.begin(), std::find(this->nodes.begin(), this->nodes.end(), end_node));
                 adj_matrix[i][j] = cost;
                 adj_matrix[j][i] = cost;
             }
         }
     }
+}
+
+using namespace std;
+int V = 12;
+
+int minDistance(int dist[], bool sptSet[])
+{
+    // Initialize min value
+    int min = INT_MAX, min_index;
+  
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+  
+    return min_index;
+}
+  
+// Function to print shortest
+// path from source to j
+// using parent array
+void printPath(int parent[], int j)
+{
+    // Base Case : If j is source
+    if (parent[j] == - 1)
+        return;
+  
+    printPath(parent, parent[j]);
+  
+    printf("%d ", j);
+}
+  
+// A utility function to print 
+// the constructed distance
+// array
+void printSolution(int dist[], int n, int parent[])
+{
+    int src = 0;
+    printf("Vertex\t Distance\tPath");
+    for (int i = 1; i < V; i++)
+    {
+        printf("\n%d -> %d \t\t %d\t\t%d ", src, i, dist[i], src);
+        printPath(parent, i);
+    }
+}
+  
+// Funtion that implements Dijkstra's
+// single source shortest path
+// algorithm for a graph represented
+// using adjacency matrix representation
+void dijkstra(int** graph, int src)
+{
+      
+    // The output array. dist[i]
+    // will hold the shortest
+    // distance from src to i
+    int dist[V]; 
+  
+    // sptSet[i] will true if vertex
+    // i is included / in shortest
+    // path tree or shortest distance 
+    // from src to i is finalized
+    bool sptSet[V];
+  
+    // Parent array to store
+    // shortest path tree
+    int parent[V];
+  
+    // Initialize all distances as 
+    // INFINITE and stpSet[] as false
+    for (int i = 0; i < V; i++)
+    {
+        parent[0] = -1;
+        dist[i] = INT_MAX;
+        sptSet[i] = false;
+    }
+  
+    // Distance of source vertex 
+    // from itself is always 0
+    dist[src] = 0;
+  
+    // Find shortest path
+    // for all vertices
+    for (int count = 0; count < V - 1; count++)
+    {
+        // Pick the minimum distance
+        // vertex from the set of
+        // vertices not yet processed. 
+        // u is always equal to src
+        // in first iteration.
+        int u = minDistance(dist, sptSet);
+  
+        // Mark the picked vertex 
+        // as processed
+        sptSet[u] = true;
+  
+        // Update dist value of the 
+        // adjacent vertices of the
+        // picked vertex.
+        for (int v = 0; v < V; v++)
+  
+            // Update dist[v] only if is
+            // not in sptSet, there is
+            // an edge from u to v, and 
+            // total weight of path from
+            // src to v through u is smaller
+            // than current value of
+            // dist[v]
+            if (!sptSet[v] && graph[u][v] &&
+                dist[u] + graph[u][v] < dist[v])
+            {
+                parent[v] = u;
+                dist[v] = dist[u] + graph[u][v];
+            } 
+    }
+  
+    // print the constructed
+    // distance array
+    printSolution(dist, V, parent);
 }
 
 int main()
@@ -121,5 +236,8 @@ int main()
         }
         std::cout << "\n";
     }
+
+    dijkstra(g.adj_matrix, 0);
+
     return 0;
 }
