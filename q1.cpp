177e7e456bc6c3c7314ab810d6fdf8af43b050ce
Enes Demirag <ensdmrg@gmail.com>
2021-04-24 00:09:14 +0300

    Comments added

--- q1.cpp
+++ q1.cpp
@@ -1,3 +1,38 @@
+/* @Author
+
+Student Name: Enes Demirag
+Student ID  : 504201571
+Mail        : ensdmrg@gmail.com
+Date		: 23.04.2021
+
+
+Dear sir/madam,
+
+This projects consist of a Graph class with methods like Minimum Spanning Tree (MST).
+Below commands can be used to build and run this project with g++.
+
+>> g++ -std=c++11 q1.cpp -o q1
+>> ./q1
+>> city_plan_1.txt
+
+All of the standard libraries used in this project is given below.
+
+<algorithm> : To find element positions inside lists and vectors.
+<iostream>  : To print things to the terminal.
+<fstream>   : To read things from a file.
+<sstream>   : To parse sub strings
+<string>    : String operations.
+<vector>    : Storing some elements and accessing them.
+<set>       : Storing unique elements.
+<map>       : Mapping pairs of elements.
+
+If you encounter any problem regarding this projects, feel free to reach me.
+
+Thanks and kind regards,
+Enes
+*/
+
+// Including necessary standard libraries.
 #include <algorithm>
 #include <iostream>
 #include <fstream>
@@ -7,16 +42,25 @@
 #include <set>
 #include <map>
 
+/* Definitions:
+ * Node is just a string,
+ * A pair of two nodes make a Connection
+ * Weighted connection is  called Edge
+ * Solution is a vector of multiple Edges.
+ */
 typedef std::string Node;
 typedef std::pair<Node, Node> Connection;
 typedef std::pair<int, Connection> Edge;
 typedef std::pair<std::vector<Edge>, int> Solution;
 
+/* This is a very simple class which stores all the nodes,
+ * all the Edges (connections with cost values), and the mapping of the directions.
+ */
 class Graph {
 private:
     std::vector<Edge> edges;
     std::set<Node> nodes;
-    std::map<Node, Node> tree;
+    std::map<Node, Node> tree; // Actually its not a tree structure. Just a dictionary of directions.
 public:
     void addNode(Node& node);
     Node& findNode(Node& node);
@@ -24,12 +68,22 @@ public:
     Solution MST();
 };
 
+/* This method adds a new node into the graph.
+ * Given node should not be already inside the graph.
+ * @param node Node to be added.
+ */
 void Graph::addNode(Node& node)
 {
+    // Insert the node into the nodes vector.
     this->nodes.insert(node);
+    // Every node is connected to itself.
     this->tree.insert(Connection(node, node));
 }
 
+/* This recursive method call itself until it finds the source of itself.
+ * @param node Node which we want to find its source
+ * @return Node source node of the given node.
+ */
 Node& Graph::findNode(Node& node)
 {
     if (node != this->tree[node])
@@ -39,6 +93,10 @@ Node& Graph::findNode(Node& node)
     return this->tree[node];
 }
 
+/* This method adds a new edge into the graph.
+ * While adding the edge, it also adds the nodes of the edge into the class attribute nodes.
+ * @param edge Edge to be added.
+ */
 void Graph::addEdge(Edge& edge)
 {
     addNode(edge.second.first);
@@ -46,15 +104,26 @@ void Graph::addEdge(Edge& edge)
     this->edges.push_back(edge);
 }
 
+/* This method is a Minimum Spanning Tree algorithm implementation.
+ * First we check every edge using loop to count for the constrains.
+ * Every iteration process will check different constrains.
+ * After obtaining constrains, at the last loop we will span the tree while pushing the nodes we visit
+ * and counting the total cost.
+ * @return Solution Found minimum spanning tree.
+ */
 Solution Graph::MST()
 {
+    // We will push every visited Edge into this vector.
     std::vector<Edge> min_spanning_tree;
+    // Total cost
     int total_cost = 0;
 
-    // Sort the edges of the graph.
+    // First, sort the edges of the graph.
     std::sort(this->edges.begin(), this->edges.end());
-    
-    // Constrain 2 and 5
+
+    // First loop constrains 2 and 5.
+    // For every edge, we look for a Great Palace - Hippodrome pair.
+    // As soon as we found it, we will add it into our solution spanning tree and break the loop.
     for (Edge& e : this->edges)
     {
         Node begin_node = e.second.first;
@@ -64,19 +133,27 @@ Solution Graph::MST()
         Node dst = findNode(end_node);
         int cost = e.first;
 
+        // If Great Palace is connected to the Hippodrome, we want it add the Edge to our tree.
         if ("GP" == begin_node && "Hipp" == end_node)
         {
+            // Mapping our way. Direction from source node to destination node.
             src = findNode(src);
             dst = findNode(dst);
             tree[dst] = src;
 
+            // Add this Edge into the solution tree.
             min_spanning_tree.push_back(Edge(cost, Connection(begin_node, end_node)));
             total_cost += cost;
+
+            // Then break the loop.
             break;
         }
     }
 
-    // Constrain 3 and 4
+    // Second loop for constrains 3 and 4.
+    // For every edge, we look for a Great Palace - Church pair.
+    // Because we previously sorted the edges, first occurrence will have the least cost.
+    // We will at it into our solution spanning tree and break the loop.
     for (Edge& e : this->edges)
     {
         Node begin_node = e.second.first;
@@ -86,96 +163,134 @@ Solution Graph::MST()
         Node dst = findNode(end_node);
         int cost = e.first;
 
+        // If Great Palace is connected to a Church, we want it add the Edge to our tree.
         if (("GP" == begin_node && "Ch" == end_node.substr(0, 2)) || ("GP" == end_node && "Ch" == begin_node.substr(0, 2)))
         {
+            // Mapping our way. Direction from source node to destination node.
             src = findNode(src);
             dst = findNode(dst);
             tree[dst] = src;
 
+            // Add this Edge into the solution tree.
             min_spanning_tree.push_back(Edge(cost, Connection(begin_node, end_node)));
+
+            // Then break the loop.
             total_cost += cost;
             break;
         }
     }
 
-    // Spanning
+    // Third loop for spanning.
+    // This is the real loop of Kruskal's algorithm. Others were implemented to achieve the given conditions.
+    // Inside this loop we check the remaining constrains and omit them.
+    // And for every edge we will span out tree to every node in the graph.
     for (Edge& e : this->edges)
     {
         Node begin_node = e.second.first;
         Node end_node = e.second.second;
 
+        // Constrain 7
+        // If both nodes of the edge are a house of an important person, we will omit this edge.
+        if ("Hp" == begin_node.substr(0, 2) && "Hp" == end_node.substr(0, 2))
+        {
+            continue;
+        }
+        // Constrain 6
+        // If a basilica is connected to the hippodrome, we will omit this edge either.
+        else if (("Hipp" == begin_node && "Bas" == end_node.substr(0, 3)) || ("Bas" == begin_node.substr(0, 3) && "Hipp" == end_node))
+        {
+            continue;
+        }
+
         Node src = findNode(begin_node);
         Node dst = findNode(end_node);
         int cost = e.first;
 
-        if ("Hp" == begin_node.substr(0, 2) && "Hp" == end_node.substr(0, 2)) // Constrain 7
+        // If two nodes are different.
+        if (src != dst)
         {
-            continue;
-        }
-        else if (("Hipp" == begin_node && "Bas" == end_node.substr(0, 3)) || ("Bas" == begin_node.substr(0, 3) && "Hipp" == end_node)) // Constrain 6
-        {
-            continue;
-        }
-        else if (src != dst)
-        {
+            // Mapping our way. Direction from source node to destination node.
             src = findNode(src);
             dst = findNode(dst);
             tree[dst] = src;
 
+            // Add this Edge into the solution tree.
             min_spanning_tree.push_back(Edge(cost, Connection(begin_node, end_node)));
             total_cost += cost;
         }
     }
 
+    // Because we add two edges with different costs inside the first two loops
+    // in order to apply the constrains. We will sort the solution tree again for the submission system.
     sort(min_spanning_tree.begin(), min_spanning_tree.end());
 
+    // Return the minimum spanning tree of edges.
     return Solution(min_spanning_tree, total_cost);
 }
 
+/* This is the main function.
+ * It takes a file name from the terminal,
+ * First creates a graph with the edges given in the file.
+ * Then find the minimum spanning tree using Kruskal's algorithm.
+ */
 int main()
 {
     // Create an empty graph.
     Graph graph;
 
-    // Read from file
+    // Define variables for paring operations.
     std::string file_name, line, str;
     std::vector<std::string> params;
 
+    // Get the input file name from terminal.
     std::cin >> file_name;
     std::ifstream city_plan(file_name);
 
-    // Fill the graph.
+    // Read from file and fill the graph with given edges.
     if (city_plan.is_open())
     {
+        // Parse every line of the file
         while (std::getline(city_plan, line))
         {
             params.clear();
             std::istringstream parser(line);
+
+            // Split the line by ',' comma character.
             while (getline(parser, str, ','))
             {
+                // Add parsed strings.
                 params.push_back(str);
             }
+
+            // Every line should have 3 strings separated with commas between them.
             if (3 == params.size())
             {
+                // Add the edge into the graph.
                 Edge e(std::stoi(params[2]), Connection(params[0], params[1]));
                 graph.addEdge(e);
             }
         }
+
+        // Close the file after finishing the read operation.
         city_plan.close();
     }
 
-    // Run the Minimum Spanning Tree algorithm.
+    // Run the Minimum Spanning Tree (MST) Kruskal's algorithm and get the solution tree.
     Solution min_spanning_tree = graph.MST();
     
-    // Print edges
+    // We already sorted this solution before coming here, but just in case we will sort again :)
     std::sort(min_spanning_tree.first.begin(), min_spanning_tree.first.end());
+
+    // For every edge inside the tree.
     for (Edge& e : min_spanning_tree.first)
     {
+        // Print the edge information.
         std::cout << e.second.first << " " << e.second.second << " " << e.first << std::endl;
     }
 
     // Print total cost
     std::cout << min_spanning_tree.second << std::endl;
 
+    // End program
     return 0;
 }
--- q2.cpp
+++ q2.cpp
@@ -1,24 +1,68 @@
-#include <cstdio>
-#include <climits>
-#include <vector>
-#include <string>
+/* @Author
+
+Student Name: Enes Demirag
+Student ID  : 504201571
+Mail        : ensdmrg@gmail.com
+Date		: 23.04.2021
+
+
+Dear sir/madam,
+
+This projects consist of a Graph class with methods like Dijkstra'a Shortest Path algorithm.
+Below commands can be used to build and run this project with g++.
+
+>> g++ -std=c++11 q2.cpp -o q2
+>> ./q2
+>> path_info_1.txt
+
+All of the standard libraries used in this project is given below.
+
+<algorithm> : To find element positions inside lists and vectors.
+<iostream>  : To print things to the terminal.
+<fstream>   : To read things from a file.
+<sstream>   : To parse sub strings
+<climits>   : To assign the maximum integer value.
+<string>    : String operations.
+<vector>    : Storing some elements and accessing them.
+<tuple>     : To store three values in a pack.
+<set>       : Storing unique elements.
+<map>       : Mapping pairs of elements.
+
+If you encounter any problem regarding this projects, feel free to reach me.
+
+Thanks and kind regards,
+Enes
+*/
+
+// Including necessary standard libraries.
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <algorithm>
-#include <map>
-#include <set>
+#include <climits>
+#include <string>
+#include <vector>
 #include <tuple>
+#include <set>
+#include <map>
 
+/* Definitions:
+ * Node is just a string,
+ * Edge is a pack of one int and two strings.
+ */
 typedef std::string Node;
-typedef std::tuple<int, std::string, std::string> Edge;
+typedef std::tuple<int, Node, Node> Edge;
 
+/* This is a very simple class which stores all the nodes,
+ * all the edges, and a 2D array which represents the adjacency matrix of the graph.
+ */
 class Graph {
 private:
     std::vector<Edge> edges;
-public:
     std::vector<Node> nodes;
-    int** adj_matrix;
+public:
+    //int** adj_matrix;
+    int adj_matrix[100][100];
     void addEdge(Edge& edge);
     void createAdjacencyMatrix();
     int minDistance(int distances[], bool visited[]);
@@ -26,14 +70,22 @@ public:
     void dijkstra();
 };
 
+/* This method adds a new edge into the graph.
+ * Also we will store the nodes of the edges in the class attribute nodes.
+ * For possible duplicate nodes to emerged, we should watch out.
+ * First I want to use set instead of a vector but accessing elements of a set with indexes is not possible.
+ * So I used vector and carefully check every node before pushing inside the vector.
+ * */
 void Graph::addEdge(Edge& edge)
 {
+    // Add the edge inside the graph's edges vector.
     this->edges.push_back(edge);
 
+    // Get both end of the edge from the tuple.
     Node begin_node = std::get<1>(edge);
     Node end_node = std::get<2>(edge);
 
-    // Only the unique nodes shoud be added.
+    // Only the unique nodes should be added. So check for duplicates.
 	if (std::find(this->nodes.begin(), this->nodes.end(), begin_node) == this->nodes.end())
 	{
         this->nodes.push_back(begin_node);
@@ -44,103 +96,153 @@ void Graph::addEdge(Edge& edge)
 	}
 }
 
-void Graph::createAdjacencyMatrix()
-{
-    // Initialize the matrix, allocate memory and set initial values.
+/* This method will be called after all the edges were push inside a vector.
+ * This method will create the adjacency matrix of the graph.
+ * We need this matrix in order to search the shortest path via Dijstra's algorithm.
+ * First I used double integer pointer int** adj_matrix to dynamically allocate enough memory space for the inputs.
+ * For the initialization I used below code section.
     *adj_matrix = new int[nodes.size()];
     for (uint x = 0; x < nodes.size(); x++)
     {
         adj_matrix[x] = new int[nodes.size()];
         std::fill_n(adj_matrix[x], nodes.size(), 0);
     }
+ * Although it works on two different linux systems I use, I got segmentation fault runtime error on the ITU server.
+ * I cannot find the real reason beneath this error, so I give up this approach and simply used a fixed size 2D array
+ * large enough to store given values. int adj_matrix[100][100].
+ */
+void Graph::createAdjacencyMatrix()
+{
+    // Initialize the adjacency matrix.
+    // Actually we do not need this part but I want to set initial value 0 to every element.
+    for (uint x = 0; x < 100; x++)
+    {
+        std::fill_n(adj_matrix[x], 100, 0);
+    }
 
-    // Put cost values
+    // For every node in the graph
     for (uint i = 0; i < nodes.size(); i++)
     {
+        // For every edge in the graph
         for (Edge& e : this->edges)
         {
+            // Unpack edge variables.
             int cost = std::get<0>(e);
             Node begin_node = std::get<1>(e);
             Node end_node = std::get<2>(e);
 
             // Enemy constrain
+            // If one of the nodes of the edge is enemy, omit this edge and not update the adjacency matrix.
+            // Actually we dont need this part because even if we wrote these costs into the adjacency matrix
+            // we are also checking this constrain inside the dijkstra method as well.
             if ("E" == begin_node.substr(0, 1) || "E" == end_node.substr(0, 1))
             {
                 continue;
             }
+            // If this edge is connected to the this node
             else if (begin_node == nodes[i])
             {
-                // Find the end node's index.
+                // Find the other node of the edge inside our unique nodes vector.
+                // I am using find() and distance() functions in order to get the index of the node.
                 int j = std::distance(this->nodes.begin(), std::find(this->nodes.begin(), this->nodes.end(), end_node));
+
+                // Write the cost to the intersections of this nodes inside the matrix.
+                // Keep in mind that this matrix is always symmetrical along the diagonals and its a square matrix
+                // with 0 values at the diagonals. Because we do not want to choose the same node again and again.
                 adj_matrix[i][j] = cost;
                 adj_matrix[j][i] = cost;
             }
         }
     }
 }
-
+/* This method looks for every edge and returns the closest one (minimum cost value)
+ * @param distances[] Array of distance values (costs) of all possible edges.
+ * @param visited[] Array of booleans which stores true if an edge was visited before and already inside the path.
+ * @return int Index of the shortest chosen edge.
+ */
 int Graph::minDistance(int distances[], bool visited[])
 {
-    // Initialize min value
+    // Initialize min value with integer limit.
     int min = INT_MAX;
     int min_index = 0;
-  
+
+    // Loop every node
     for (uint v = 0; v < this->nodes.size(); v++)
     {
-        if (visited[v] == false && distances[v] <= min)
+        // If it was not already visited and closer than previous edge.
+        if (!visited[v] && distances[v] <= min)
         {
-             min = distances[v];
-             min_index = v;
+            // Write this edge's index.
+            min = distances[v];
+            min_index = v;
         }
     }
+
+    // After iterating over every possible edge, return the index of closest.
     return min_index;
 }
 
-void Graph::printPath(int parent[], int node_index)
+/* Printing method for the question. Prints the travelled route from beginning to the end.
+ * This recursive method will called from the end node and calls itself over and over again
+ * until it reaches the beginning node (whose parent is -1) and starts printing while going back on the call stack.
+ * @param parents[] Index array of parent nodes.
+ * @param node_index Index of the current node.
+ * */
+void Graph::printPath(int parents[], int node_index)
 {
     // If start node reached.
-    if (parent[node_index] == - 1)
+    if (parents[node_index] == - 1)
     {
         std::cout << this->nodes[node_index] << " ";
         return;
     }
 
     // Recursive call to the parent
-    printPath(parent, parent[node_index]);
+    printPath(parents, parents[node_index]);
     std::cout << this->nodes[node_index] << " ";
 }
-  
+
+/* This method is the implementation of the Dijkstra's Shortest Path algorithm.
+ *
+ * */
 void Graph::dijkstra()
 {
-    int dist[this->nodes.size()]; 
-    bool sptSet[this->nodes.size()];
-    int parent[this->nodes.size()];
-  
+    // For every nodes initial values are set.
+    int distances[this->nodes.size()];
+    bool visited[this->nodes.size()];
+    int parents[this->nodes.size()];
+    parents[0] = -1;
+
     for (uint i = 0; i < this->nodes.size(); i++)
     {
-        parent[0] = -1;
-        dist[i] = INT_MAX;
-        sptSet[i] = false;
+        distances[i] = INT_MAX;
+        visited[i] = false;
     }
-  
-    dist[0] = 0;
-  
+
+    distances[0] = 0;
+
     for (uint count = 0; count < this->nodes.size() - 1; count++)
     {
-        int u = minDistance(dist, sptSet);
-  
-        sptSet[u] = true;
-  
+        // Find the closest node.
+        int u = minDistance(distances, visited);
+
+        // Make it visited.
+        visited[u] = true;
+
+        // For every nodes, look for connections.
         for (uint v = 0; v < this->nodes.size(); v++)
   
-            // Visited degilse ve ulasim varsa ve 
-            if (!sptSet[v] && this->adj_matrix[u][v] && dist[u] + this->adj_matrix[u][v] < dist[v])
+            // If it is not visited yet and the next node is closer.
+            if (!visited[v] && this->adj_matrix[u][v] && distances[u] + this->adj_matrix[u][v] < distances[v])
             {
+                // Check for the enemy condition.
+                // This if will not even become true. Because we did not wrote the costs of the enemies. (they were omited at the beginning).
                 if (this->nodes[u].substr(0, 1) == "E")
                 {
                     continue;
                 }
 
+                // Look at every edge connected to the node u and check if it has any enemy node closer than 5.
                 bool smaller_than_5 = false;
                 for (Edge e : this->edges)
                 {
@@ -155,66 +257,81 @@ void Graph::dijkstra()
                     }
                 }
 
+                // If there is a enemy within 5 value, ignore this node and continue.
                 if (smaller_than_5)
                 {
                     continue;
                 }
-                
-                parent[v] = u;
-                dist[v] = dist[u] + this->adj_matrix[u][v];
+
+                // Append new node v to the current node u.
+                parents[v] = u;
+
+                // Update the distance value of the current node.
+                distances[v] = distances[u] + this->adj_matrix[u][v];
             } 
     }
-  
-    printPath(parent, this->nodes.size() - 1);
-    std::cout << dist[this->nodes.size() - 1] << std::endl;
+
+    // Print the full route.
+    printPath(parents, this->nodes.size() - 1);
+
+    // Print the total distance value.
+    std::cout << distances[this->nodes.size() - 1] << std::endl;
 }
 
+/* This is the main function.
+ * It takes a file name from the terminal,
+ * First creates a graph with the edges given in the file.
+ * Then find the shortest path from them first node the the end node.
+ * using Dijkstra's algorithm.
+ */
 int main()
 {
     // Create an empty graph.
     Graph g;
 
-    // Read from file
-    std::string file_name;
-    std::string line;
-    std::string str;
+    // Define variables for paring operations.
+    std::string file_name, line, str;
     std::vector<std::string> params;
 
-    //std::cin >> file_name;
-    std::ifstream city_plan("path_info_1.txt");
+    // Get the input file name from terminal.
+    std::cin >> file_name;
+    std::ifstream road_map(file_name);
 
-    // Fill the graph.
-    if (city_plan.is_open())
+    // Read from file and fill the graph with given edges.
+    if (road_map.is_open())
     {
-        while (std::getline(city_plan, line))
+        // Parse every line of the file
+        while (std::getline(road_map, line))
         {
             params.clear();
             std::istringstream parser(line);
+
+            // Split the line by ',' comma character.
             while (getline(parser, str, ','))
             {
+                // Add parsed strings.
                 params.push_back(str);
             }
+
+            // Every line should have 3 strings separated with commas between them.
             if (3 == params.size())
             {
+                // Add the edge into the graph.
                 Edge e(std::stoi(params[2]), params[0], params[1]);
                 g.addEdge(e);
             }
         }
-        city_plan.close();
+
+        // Close the file after finishing the read operation.
+        road_map.close();
     }
 
+    // Create the adjacency matrix after adding every edge into the graph.
     g.createAdjacencyMatrix();
 
-    for (int i = 0; i < 12; i++)
-    {
-        for (int j = 0; j < 12; j++)
-        {
-            std::cout << g.adj_matrix[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
-
+    // Run Dijkstra's Shortest Path Algorithm.
     g.dijkstra();
 
+    // End program
     return 0;
 }
